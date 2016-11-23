6d5bd194725f710891514d683654ffb1284db437
jstuck <justin.stuck@gmail.com>
2016-11-23 14:56:51 -0600

    Implemented motion and face detectors. Allowed for passing of camera control between states

--- states.py
+++ states.py
@@ -1,10 +1,25 @@
 import pickle
 import pyttsx
+import cv2
+import time
 from motion_detector import MotionDetector 
-
+from face_detector import FaceDetector
 class State(object):
     def identify(self):
         print "Currently helping ", self.attending
+    def proceed(self):
+        pass
+'''		
+    def action(self)
+        pass
+'''
+    def revert(self):
+        pass
+        
+        
+#Refactor State class to force implementation of an action() method
+# in order to make state transitions and actions more object oriented/pythonic
+
 
 class DetectingMotion(State):
 
@@ -13,35 +28,41 @@ class DetectingMotion(State):
         self.activity = "Detecting Motion..."
     def detect_motion(self):
         print self.activity
-        self.jarvis.motion_detector.detect_motion()
+        self.jarvis.motion_detector.detect_motion(self.jarvis.camera)
         print "Detected Motion"
         self.proceed()
             
-            
     def proceed(self):
         self.jarvis.state=self.jarvis.scanningstate
-        
+        self.jarvis.state.detect_faces()
     def revert(self):
         return 0 #This is the base state
 
+        
 class Scanning(State):
     def __init__(self,jarvis):
         self.jarvis = jarvis
         self.activity = "Scanning for Faces..."
     def detect_faces():
         print self.activity
+        faces = 
+        
+        
     def proceed(self):
         self.jarvis.state =  self.jarvis.facestate
-
+        self.jarvis.state.classify_face(faces)
     def revert(self):
         self.jarvis.state = self.jarvis.detectingstate
 
+        
 class FacialRecognition(State):
     def __init__(self,jarvis):
         self.jarvis = jarvis
         self.activity = "Classifying faces..."
         self.classifier = pickle.load(open('Faces.pkl', 'rb'))
-    def classify_face():
+        
+    def classify_face(faces):
+        names = self.classifier.predict(faces)
         
         print self.activty
         
@@ -49,6 +70,8 @@ class FacialRecognition(State):
         self.jarvis.state = self.jarvis.greetingstate
     def revert(self):
         self.jarvis.state = self.jarvis.detectingstate
+
+        
 class GreetRoommate(State):
     def __init__(self,jarvis):
         
@@ -68,6 +91,8 @@ class GreetRoommate(State):
         self.jarvis.state = self.jarvis.waitingstate
     def revert(self):
         self.jarvis.state = self.jarvis.facestate
+
+        
 class WaitingForTask(State):
     def __init__(self,jarvis):
         self.jarvis = jarvis
@@ -77,6 +102,8 @@ class WaitingForTask(State):
         self.jarvis.state = self.jarvis.servingstate
     def revert(self):
         self.jarvis.state = self.jarvis.detectingstate
+
+        
 class Serving(State):
     def __init__(self,jarvis):
         self.jarvis = jarvis
@@ -94,9 +121,13 @@ class Jarvis(object):
         #observer = Observable()
         #motion_detector = MotionObserver('Motion Detector')
         #observer.register(motion_detector)
+        self.camera = cv2.VideoCapture(0)
+        time.sleep(0.5)
         self.motion_detector = MotionDetector()
+        self.face_detector = FaceDetector()
         self.detectingstate = DetectingMotion(self)
         self.scanningstate = Scanning(self)
+        
         self.facestate = FacialRecognition(self)
         self.greetingstate = GreetRoommate(self)
         self.waitingstate = WaitingForTask(self)
